7ee18345f09b763a36fd62d6970b94b48631a143
Trevor Locke <locke.trevor@gmail.com>
2021-01-29 20:16:20 -0600

    login functionality complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import {Login} from './Components/Login';
+import Login from "./Components/Login";
 import {Signup} from './Components/Signup';
 import {MainPage} from './Components/MainPage';
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={MainPage} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/signup" component={Signup} />
-    </Router>
-  );
+		<Router>
+			<Route exact path="/" component={MainPage} />
+			<Route exact path="/login" component={Login} />
+			<Route exact path="/signup" component={Signup} />
+		</Router>
+	);
 }
 
 export default App;
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,49 @@
-import React, { useState } from "react";
 
-export function Login(){
-  return(
-    <div>Login</div>
-  )
-}
+
+
+import { React, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import "./../App.scss";
+
+const Login = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	function validateForm() {
+		return email.length > 0 && password.length > 0;
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+	}
+
+	return (
+		<div>
+			<Form onSubmit={handleSubmit}>
+				<FormGroup size="lg" controlId="email">
+					<Input
+						autoFocus
+						type="email"
+            placeholder="Username"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</FormGroup>
+				<FormGroup size="lg" controlId="password">
+					<Input
+						type="password"
+            placeholder="Password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</FormGroup>
+				<Button block size="lg" type="submit" disabled={!validateForm()}>
+					Login
+				</Button>
+			</Form>
+		</div>
+	);
+};
+
+export default Login;
