0c93832da6a5cab5f2e30f7c3954ecccfd5788ff
IsaacGHoward <howardisaac100@gmail.com>
2021-01-29 17:54:37 -0800

    Added sample How-To data, that can be viewed from /home page

--- src/Components/UserHome.js
+++ src/Components/UserHome.js
@@ -2,12 +2,26 @@ import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 
+import {HowTo} from './HowTo'
+
 export function UserHome(){
+  //This data should be fetched from the server using an axios get request in a useEffect hook that runs on first render
+  const [howtoData, setHowToData] = useState([
+    {title: "How to brush your teeth", author: "Anonymous", content: "So basically you get a toothbrush", id:0},
+    {title: "10 crazy cooking tips!", author: "Epic Youtube Vidz", content: "Click the link to view the video!", id:1},
+    {title: "Other uses for toothpicks", author: "testuser", content: "They aren't just for cleaning your teeth.", id:2},
+  ])
   return(
     <div>
       User Home
       <Link to="/create">Create How-To</Link>
-      <Link to="/howto/5">How To #5</Link>
+      {
+        howtoData.map(howto => (
+          <Link key={howto.id} to={`/howto/${howto.id}`}>
+            <HowTo data={howto} />
+          </Link>
+        ))
+      }
     </div>
   )
 }
