5a37d83278e055afb31f63abf25b359005514c42
IsaacGHoward <howardisaac100@gmail.com>
2021-02-04 17:25:35 -0800

    Added more fields to the HowToForm

--- src/Components/HowToForm.js
+++ src/Components/HowToForm.js
@@ -11,17 +11,23 @@
 
 import React, { Component } from 'react';
 
+import {axiosWithAuth} from './Util/axiosWithAuth';
+
 export class HowToForm extends Component {
     constructor(){
         super();
         this.onSubmit = this.onSubmit.bind(this);
     }
     state = {
-        title: ''
+        title: '',
+        problem: '',
+        solution: '',
+        username: JSON.parse(localStorage.getItem('user')).username,
+        user_id: JSON.parse(localStorage.getItem('user')).id
     }
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.HowToForm(this.state.title);
+        //this.props.HowToForm(this.state.title);
         this.setState({title: ''});
     }
     onChange = (e) => this.setState({ [e.target.name]:  e.target.value});
@@ -31,9 +37,25 @@ export class HowToForm extends Component {
                <input 
                type='text'
                name='title'
-               placeholder='Add How To ...'
+               placeholder='Title'
                style={{flex: '10', padding: '5px'}}
                value={this.state.title}
+               onChange={this.onChange}
+               />
+               <input 
+               type='text'
+               name='problem'
+               placeholder='Problem'
+               style={{flex: '10', padding: '5px'}}
+               value={this.state.problem}
+               onChange={this.onChange}
+               />
+               <input 
+               type='text'
+               name='solution'
+               placeholder='Solution'
+               style={{flex: '10', padding: '5px'}}
+               value={this.state.solution}
                onChange={this.onChange}
                />
                <input
